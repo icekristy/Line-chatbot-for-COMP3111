8dfe71970222e56756ea61ed3c1d7abcbe7f0000
Kristy Fung <icekristy@Kristys-MacBook-Air.local>
2017-09-28 18:44:50 +0800

    lab3test21

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
@@ -12,33 +12,22 @@ public class SQLDatabaseEngine extends DatabaseEngine {
 	@Override
 	String search(String text) throws Exception {
 		//Write your code here
-		ResultSet result = null;
-		 Connection connection = null;
-		 PreparedStatement stmt = null;
-		 
-		 try {
-		 		connection = this.getConnection();
-		 		stmt = connection.prepareStatement(
-		 				"select reponse from test where keyword like concat('%', ?, '%')"
-		 				);
-		 		stmt.setString(1, text); 
-		 		result = stmt.executeQuery();
-		 		if (result.next())
-		 		return result.getString(1);
-		 		System.out.println("result: " + result.toString());
-		 } catch (Exception e) {
-		 	log.info("IOException while reading file: {}", e.toString());
-		 } finally {
-		 	try {
-		 		result.close();stmt.close();connection.close();
-		 	} catch (Exception ex) {
-		 		log.info("IOException while closing file: {}", ex.toString());
-		 	}
-		 }
-		 
-		 if (result.next())
-		 	return result.getString(1);
-		 throw new Exception("NOT FOUND");
+		Connection connection = getConnection();
+		PreparedStatement stmt=connection.prepareStatement("Select response from test where keyword = ?");
+		stmt.setString(1,text);
+		ResultSet rs = stmt.executeQuery();
+		String result = null;
+		while(rs.next()) {
+			result = rs.getString(1);
+		}
+		
+		rs.close();
+		stmt.close();
+		connection.close();
+		if(result!=null)
+			return result;
+		
+		throw new Exception ("Not found");
 
 	}
 	
